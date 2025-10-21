800a21312ecb697fe5ff90bea9ebf9f5d29c793b
NANA7390 <zuotengyumei93@gmail.com>
2025-10-21 21:08:59 +0900

    fix bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,8 @@
     {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 
 
-<link rel="stylesheet" href="{{ asset('app-DWpGJWH7.css') }}">
-<script src="{{ asset('js/app-CdQXwo7F.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script src="{{ asset('js/app.js') }}"></script>
 
 </head>
 <body>
